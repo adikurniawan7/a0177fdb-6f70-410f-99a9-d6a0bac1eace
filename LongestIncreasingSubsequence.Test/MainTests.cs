8bd09419b62abeedee33823f0a26f720b6a2bb1f
adikurniawan7 <adikurniawan85@gmail.com>
2025-12-14 13:58:07 +1100

    Add function to find longest increasing subsequence of a string.
    * Overload FindLongestIncreasingSubsequence function
    * Added unit tests to test data and edge cases
    * Added testData to separate unit tests and test data

---
+++ LongestIncreasingSubsequence.Test/MainTests.cs
@@ -0,0 +1,120 @@
+﻿using LongestIncreasingSubsequence.Test.TestData;
+
+namespace LongestIncreasingSubsequence.Test;
+
+public class MainTests
+{
+    [Theory]
+    [MemberData(nameof(StringCases))]
+    public void FindLongestIncreasingSubsequence_StringInput_ReturnsExpected(string input, int[] expected)
+    {
+        var result = Main.FindLongestIncreasingSubsequence(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(ConvertStringCases))]
+    public void ConvertStringToArrayOfInteger_ReturnsExpected(string input, int[] expected)
+    {
+        var result = Main.ConvertStringToArrayOfInteger(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [MemberData(nameof(Cases))]
+    public void FindLongestIncreasingSubsequence_ReturnsExpected(int[] input, int[] expected)
+    {
+        var result = Main.FindLongestIncreasingSubsequence(input);
+        Assert.Equal(expected, result);
+    }
+
+    public static IEnumerable<object[]> StringCases
+    {
+        get
+        {
+            // edge cases
+            yield return new object[] { "", Array.Empty<int>() };
+            yield return new object[] { "     ", Array.Empty<int>() };
+            yield return new object[] { "ABC DEF", Array.Empty<int>() };
+            yield return new object[] { "99999999999999999999", Array.Empty<int>() };
+            yield return new object[] { "99999999999999999999 123", new int[] { 123 } };
+            yield return new object[] { "1", new int[] { 1 } };
+            yield return new object[] { "1 2 3 4 5", new int[] { 1, 2, 3, 4, 5 } };
+            yield return new object[] { "   1     2     3     4     5    ", new int[] { 1, 2, 3, 4, 5 } };
+            yield return new object[] { "a 2 c 4 e", new int[] { 2, 4 } };
+            yield return new object[] { "5 4 3 2 1", new int[] { 5 } };
+
+            // testdata
+            yield return new object[] { LisTestData.StringTestData1, LisTestData.OutputTestData1 };
+            yield return new object[] { LisTestData.StringTestData2, LisTestData.OutputTestData2 };
+            yield return new object[] { LisTestData.StringTestData3, LisTestData.OutputTestData3 };
+            yield return new object[] { LisTestData.StringTestData4, LisTestData.OutputTestData4 };
+            yield return new object[] { LisTestData.StringTestData5, LisTestData.OutputTestData5 };
+            yield return new object[] { LisTestData.StringTestData6, LisTestData.OutputTestData6 };
+            yield return new object[] { LisTestData.StringTestData7, LisTestData.OutputTestData7 };
+            yield return new object[] { LisTestData.StringTestData8, LisTestData.OutputTestData8 };
+            yield return new object[] { LisTestData.StringTestData9, LisTestData.OutputTestData9 };
+            yield return new object[] { LisTestData.StringTestData10, LisTestData.OutputTestData10 };
+            yield return new object[] { LisTestData.StringTestData11, LisTestData.OutputTestData11 };
+        }
+    }
+
+    public static IEnumerable<object[]> ConvertStringCases
+    {
+        get
+        {
+            // edge cases
+            yield return new object[] { "", Array.Empty<int>() };
+            yield return new object[] { "     ", Array.Empty<int>() };
+            yield return new object[] { "ABC DEF", Array.Empty<int>() };
+            yield return new object[] { "99999999999999999999", Array.Empty<int>() };
+            yield return new object[] { "99999999999999999999 123", new int[] { 123 } };
+            yield return new object[] { "1", new int[] { 1 } };
+            yield return new object[] { "1 2 3 4 5", new int[] { 1, 2, 3, 4, 5 } };
+            yield return new object[] { "   1     2     3     4     5    ", new int[] { 1, 2, 3, 4, 5 } };
+            yield return new object[] { "a 2 c 4 e", new int[] { 2, 4 } };
+            yield return new object[] { "5 4 3 2 1", new int[] { 5, 4, 3, 2, 1 } };
+
+            // testdata
+            yield return new object[] { LisTestData.StringTestData1, LisTestData.TestData1 };
+            yield return new object[] { LisTestData.StringTestData2, LisTestData.TestData2 };
+            yield return new object[] { LisTestData.StringTestData3, LisTestData.TestData3 };
+            yield return new object[] { LisTestData.StringTestData4, LisTestData.TestData4 };
+            yield return new object[] { LisTestData.StringTestData5, LisTestData.TestData5 };
+            yield return new object[] { LisTestData.StringTestData6, LisTestData.TestData6 };
+            yield return new object[] { LisTestData.StringTestData7, LisTestData.TestData7 };
+            yield return new object[] { LisTestData.StringTestData8, LisTestData.TestData8 };
+            yield return new object[] { LisTestData.StringTestData9, LisTestData.TestData9 };
+            yield return new object[] { LisTestData.StringTestData10, LisTestData.TestData10 };
+            yield return new object[] { LisTestData.StringTestData11, LisTestData.TestData11 };
+        }
+    }
+
+    public static IEnumerable<object[]> Cases
+    {
+        get
+        {
+            // edge cases
+            yield return new object[] { LisTestData.Empty, Array.Empty<int>() };
+            yield return new object[] { LisTestData.One, new int[] { 1 } };
+            yield return new object[] { LisTestData.Ascending, new int[] { 1, 2, 3, 4, 5, 6, 7 } };
+            yield return new object[] { LisTestData.Descending, new int[] { 7 } };
+            yield return new object[] { LisTestData.TwoAscending, new int[] { 1, 2 } };
+            yield return new object[] { LisTestData.TwoDescending, new int[] { 2 } };
+            yield return new object[] { LisTestData.AllSame, new int[] { 7 } };
+
+            // test data
+            yield return new object[] { LisTestData.TestData1, LisTestData.OutputTestData1 };
+            yield return new object[] { LisTestData.TestData2, LisTestData.OutputTestData2 };
+            yield return new object[] { LisTestData.TestData3, LisTestData.OutputTestData3 };
+            yield return new object[] { LisTestData.TestData4, LisTestData.OutputTestData4 };
+            yield return new object[] { LisTestData.TestData5, LisTestData.OutputTestData5 };
+            yield return new object[] { LisTestData.TestData6, LisTestData.OutputTestData6 };
+            yield return new object[] { LisTestData.TestData7, LisTestData.OutputTestData7 };
+            yield return new object[] { LisTestData.TestData8, LisTestData.OutputTestData8 };
+            yield return new object[] { LisTestData.TestData9, LisTestData.OutputTestData9 };
+            yield return new object[] { LisTestData.TestData10, LisTestData.OutputTestData10 };
+            yield return new object[] { LisTestData.TestData11, LisTestData.OutputTestData11 };
+        }
+    }
+}
---
+++ LongestIncreasingSubsequence/Main.cs
@@ -0,0 +1,55 @@
+﻿namespace LongestIncreasingSubsequence;
+
+public class Main
+{
+    public static int[] FindLongestIncreasingSubsequence(string input)
+    {
+        var numbers = ConvertStringToArrayOfInteger(input);
+
+        return FindLongestIncreasingSubsequence(numbers);
+    }
+
+    public static int[] ConvertStringToArrayOfInteger(string input)
+    {
+        var substrings = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        List<int> numbers = [];
+        foreach (string substring in substrings)
+        {
+            if (int.TryParse(substring, out int number))
+            {
+                numbers.Add(number);
+            }
+        }
+        return numbers.Count > 0 ? [.. numbers] : [];
+    }
+
+    public static int[] FindLongestIncreasingSubsequence(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0) return [];
+        if (numbers.Length == 1) return [numbers[0]];
+
+        var result = new List<int> { numbers[0] };
+        var current = new List<int> { numbers[0] };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (current.Last() < numbers[i])
+            {
+                current.Add(numbers[i]);
+                // compare
+                if (current.Count > result.Count)
+                {
+                    result = [.. current];
+                }
+            }
+            else if (i < numbers.Length - 1)
+            {
+                // reset
+                current.Clear();
+                current.Add(numbers[i]);
+            }
+        }
+
+        return [.. result];    
+    }
+}
